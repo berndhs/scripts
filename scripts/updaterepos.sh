7bdd2b5f466887bb496449b139ac4f4ec6f0272a
Bernd Stramm <bernd.stramm@gmail.com>
2011-08-09 13:28:57 -0400

    allow different distros

--- scripts/updaterepos.sh
+++ scripts/updaterepos.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-REPO=/var/www/html/sites/bernd2009/Repos/Geuzen/MeeGo/current
-echo "---> creating repodata"
-createrepo $REPO
-echo "---> fixing permissions"
-chmod a+rx $REPO/repodata
-chmod a+r $REPO/repodata/*
+
+do_update () {
+  REPO=$1
+  echo "---> creating repodata for $REPO"
+  createrepo $REPO
+  echo "---> fixing permissions"
+  chmod a+rx $REPO/repodata
+  chmod a+r $REPO/repodata/*
+}
+
+if [ $# -lt 1 ]; then
+  do_update /var/www/html/sites/bernd2009/Repos/Geuzen/MeeGo/current
+  do_update /var/www/html/sites/bernd2009/Repos/Geuzen/Fedora/current
+else
+  DISTRO=$1
+  do_update  /var/www/html/sites/bernd2009/Repos/Geuzen/$DISTRO/current
+fi
+
 echo "---> all done"
 
