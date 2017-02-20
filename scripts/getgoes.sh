1417e18da7cc654d0d2c07f891ed6a0428d7271e
Bernd Stramm <bernd.stramm@gmail.com>
2017-02-20 07:18:43 -0500

    details - qmake version and stuff

--- scripts/getgoes.sh
+++ scripts/getgoes.sh
@@ -4,13 +4,23 @@ NOW=`date --utc`
 cd ~/public_html/daily/
 
 rm ECIR.JPG
-wget http://www.goes.noaa.gov/GIFS/ECIR.JPG 
+curl -o ECIR.JPG  http://www.goes.noaa.gov/GIFS/ECIR.JPG 
+echo $? >> ~/public_html/daily/done
 ls -l ECIR*
 pwd
-php ~/bin/storeIR.php ~/public_html/daily/ECIR.JPG
+#php ~/bin/storeIR.php ~/public_html/daily/ECIR.JPG
+php ~/bin/storeSQLi.php ~/public_html/daily/ECIR.JPG
+echo $? >> ~/public_html/daily/done
+ls -l ECIR.J* >> ~/public_html/daily/done
 
 rm WCIR.JPG
-wget http://www.goes.noaa.gov/GIFS/WCIR.JPG
+curl -o WCIR.JPG  http://www.goes.noaa.gov/GIFS/WCIR.JPG
+echo $? >> ~/public_html/daily/done
 ls -l WCIR*
 pwd
-php ~/bin/storeIR.php ~/public_html/daily/WCIR.JPG
+#php ~/bin/storeIR.php ~/public_html/daily/WCIR.JPG
+php ~/bin/storeSQLi.php ~/public_html/daily/WCIR.JPG
+echo $? >> ~/public_html/daily/done
+ls -l WCIR.J* >> ~/public_html/daily/done
+
+date >> ~/public_html/daily/done
